8eb52788a8281bee49287b70d05152b3b46849bc
onyx <jay@onyx-international.com>
2020-06-11 14:24:00 +0800

    onyx:jax push

--- app/src/main/java/com/example/com/sample/MainActivity.java
+++ app/src/main/java/com/example/com/sample/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.print("onyx");
+        System.out.print("funJaxBrunch");
 
     }
 }
